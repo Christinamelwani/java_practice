9f91690a31e438ee4f7c669d01c252ab15c0eb86
Christina <90821809+Christinamelwani@users.noreply.github.com>
2023-06-16 11:58:36 +0700

    Add files via upload

---
+++ Java/ByeShortIntLong/ByteShortIntLong/src/ByteShortIntLong/Main.java
@@ -0,0 +1,21 @@
+package ByteShortIntLong;
+public class Main {
+    public static void main(String[] args) {
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer minimum value = " + myMinIntValue);
+        System.out.println("Integer maximum  value = " + myMaxIntValue);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum  value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum  value = " + myMaxShortValue);
+
+    }
+}
